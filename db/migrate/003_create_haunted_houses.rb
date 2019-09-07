f3aeaaa995d68b417f2c4ddba12fc67e4f0dfe3e
Kevin Karma <kevinkarma55@gmail.com>
2019-09-07 17:39:38 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name 
+      t.string :location 
+      t.string :theme
+      t.decimal :price 
+      t.boolean :still_in_business
+      t.timestamps
+      t.string :description
+    end
+  end
+end
